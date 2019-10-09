b9307526c8f440915d60654d4b003eba0cbb08bb
KAI_Monmoy <aftarulislam@gmail.com>
2019-10-09 15:18:01 +0600

    :construction: Converting StateLess -> StateFul!

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
+          centerTitle: true,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,38 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice5.png'),
+                onPressed: () {
+                  print('Left button got pressed!');
+                },
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice6.png'),
+                onPressed: () {
+                  print('Right button got pressed!');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
